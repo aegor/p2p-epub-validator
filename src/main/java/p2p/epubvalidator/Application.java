d5ae466c6a7b15fa0483c32d088ab0f9ba84b596
Igor Akulov <akulovigor@gmail.com>
2017-10-05 21:01:26 +0500

    delete unused stuff

--- src/main/java/p2p/epubvalidator/Application.java
+++ src/main/java/p2p/epubvalidator/Application.java
@@ -6,22 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import p2p.epubvalidator.storage.StorageProperties;
-import p2p.epubvalidator.storage.StorageService;
-
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    CommandLineRunner init(StorageService storageService) {
-        return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
-        };
-    }
+    
 }
